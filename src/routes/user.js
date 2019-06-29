36c63cb4b68592d5259492e4b60fa3d920c0e054
George Zografos <george.zografos@dazn.com>
2019-06-29 09:50:16 +0200

    Move routes folder Add express jwt

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const jsYaml = require('js-yaml');
+const jwtMiddleware = require('express-jwt');
 const { OpenApiValidator } = require('express-openapi-validate');
 
 const { Router } = require('express');
-const { User } = require('./models');
+const { User } = require('../models');
 
 const openApiDocument = jsYaml.safeLoad(
   fs.readFileSync(path.resolve(__dirname, 'openapi.yaml'), 'utf-8')
@@ -12,11 +13,12 @@ const openApiDocument = jsYaml.safeLoad(
 
 const validator = new OpenApiValidator(openApiDocument);
 
-const routerUser = Router();
+const router = Router();
 
-routerUser.get(
-  '/:id',
-  validator.validate('get', '/user/{id}'),
+router.get(
+  '/get/:id',
+  validator.validate('get', '/user/get/{id}'),
+  jwtMiddleware({ secret: 'sharedSecret' }),
   async (req, res, next) => {
     const user = await User.get(req.params.id);
     if (!user) {
@@ -27,6 +29,11 @@ routerUser.get(
   }
 );
 
-module.exports = {
-  user: routerUser
-};
+router.post(
+  '/sign-up',
+  validator.validate('post', '/user/sign-up'),
+  jwtMiddleware({ secret: 'sharedSecret', credentialsRequired: false }),
+  async (req, res) => {}
+);
+
+module.exports = router;
